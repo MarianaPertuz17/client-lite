72b89126bbee3f81e9cc160464f720e5bf3104fc
Mariana <marianapertuzv@gmail.com>
2022-11-10 17:01:59 -0500

    feat: successfully toggling between buttons in dashboard

--- src/components/dashboardItem/index.tsx
+++ src/components/dashboardItem/index.tsx
@@ -3,14 +3,14 @@ import styles from './styles.module.css';
 interface Props {
   icon: string;
   title: string;
+  handleClick: (option: string) => any;
 }
 
-export function DashboardItem ({icon, title}: Props) {
-  
+export function DashboardItem ({icon, title, handleClick}: Props) {  
   return (
-    <div className={styles.container}>
+    <button className={styles.container} onClick={() => handleClick(title)}>
       <img alt={title} src={icon} className={styles.img}/>
       <p>{title}</p>
-    </div>
+    </button>
   )
 }
--- src/screens/dashboard/index.tsx
+++ src/screens/dashboard/index.tsx
@@ -3,9 +3,17 @@ import { NavBar } from '../../components/navBar';
 import { DashboardItem } from '../../components/dashboardItem';
 import companyIcon from '../../assets/images/company-icon.png';
 import plusIcon from '../../assets/images/plus-icon.png';
+import { useState } from 'react';
 
 
 export function Dashboard () {
+
+  const [ showCompanyForm, setShowCompanyForm ] = useState(false);
+
+  const handleClick = (option: string) => {
+    if (option === 'Explore companies') setShowCompanyForm(false);
+    else setShowCompanyForm(true);
+  }
 
   return (
     <>
@@ -13,12 +21,15 @@ export function Dashboard () {
       <div className={styles.container}>
         <div className={styles.optionsBar}>
           <h3>Reports</h3>
-          <DashboardItem icon={companyIcon} title='Explore companies'/>
-          <DashboardItem icon={plusIcon} title='Add new company'/>
+          <DashboardItem icon={companyIcon} title='Explore companies' handleClick={handleClick}/>
+          <DashboardItem icon={plusIcon} title='Add new company' handleClick={handleClick}/>
         </div>
         <div>
-          <h2 className={styles.h2}>Currently, these companies are available:</h2>
-
+          { !showCompanyForm ? 
+            <h2 className={styles.h2}>Currently, these companies are available:</h2>
+            :
+            <h2 className={styles.h2}>Showing form</h2>
+          }
         </div>
         
       </div>
