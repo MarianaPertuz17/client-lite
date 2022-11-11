9c9c3eb28967a1973528abb30e92bb999ded1127
MarianaPertuz17 <marianapertuzv@gmail.com>
2022-11-11 08:58:24 -0500

    feat: inventory table added

--- src/components/itemsTable/index.tsx
+++ src/components/itemsTable/index.tsx
@@ -1,32 +1,29 @@
-import { ItemAttributes } from '../../interfaces';
+import { Product } from '../../interfaces';
 import styles from './styles.module.css';
 import { useState } from 'react';
 import { grey } from '@mui/material/colors';
 import { DataGrid, GridColDef, gridClasses } from '@mui/x-data-grid';
 
 interface Props {
-  companies: ItemAttributes[];
+  items: Product[];
 }
 
-export function ItemsTable ({companies}: Props) {
+export function ItemsTable ({items}: Props) {
   
   const [pageSize, setPageSize] = useState(5);
   const [rowId, setRowId] = useState(null);
-  console.log(companies, 'las que recibo')
+  console.log(items, 'las que recibo')
 
   const columns: GridColDef[] = [
-    { field: 'name', headerName: 'Name',  width: 200 },
-    { field: 'NIT', headerName: 'NIT', width: 220 },
-    { field: 'phone', headerName: 'Phone', width: 220 },
-    { field: 'address', headerName: 'Address', width: 200 },
-    { field: 'actions', headerName: 'Actions', width: 160 },
+    { field: 'productName', headerName: 'Name',  width: 500 },
+    { field: 'quantity', headerName: 'Quantity', width: 500 },
   ];
 
   return (
     <DataGrid
       columns={columns}
-      rows={companies}
-      getRowId={(row) => row.NIT}
+      rows={items}
+      getRowId={(row) => row.id}
       rowsPerPageOptions={[5, 10, 20]}
       pageSize={pageSize}
       onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
--- src/screens/inventory/index.tsx
+++ src/screens/inventory/index.tsx
@@ -6,10 +6,30 @@ import { guestService } from '../../services/guestService';
 import { ItemAttributes } from '../../interfaces';
 import { useParams } from 'react-router-dom'
 import { Header } from '../../components/companyHeader';
+import { ItemsTable } from '../../components/itemsTable';
 
 
 export function Inventory () {
+  const [ company, setCompany ] = useState<ItemAttributes>();
+  const { nit } = useParams();
+
+  useEffect(() => {
+    fetchCompany();
+  }, []);
+
+ 
+  const fetchCompany = async() => {
+    const loggedUserJSON = localStorage.getItem('token');
+    if (loggedUserJSON && nit){
+      const {res, error} = await guestService.getCompany(loggedUserJSON, nit);
+      if (!error) {
+        setCompany(res);
+      } else alert(res);
+    }
+  }
   return (
-    <h1> HOLA </h1>    
+    <div style={{height:'20vh'}}>
+    {company?.products && <ItemsTable items={company.products}/>}
+    </div>
   )
 }
